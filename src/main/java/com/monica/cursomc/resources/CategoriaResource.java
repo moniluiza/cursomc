62c95766f2a553e6c608b046944c8db7357adba8
Monica Luiza <moniluizad@gmail.com>
2022-04-13 17:37:43 -0300

    Testando a Categoria

--- src/main/java/com/monica/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/monica/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.monica.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.monica.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias") // esse eh o EndPoint
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 
 }
