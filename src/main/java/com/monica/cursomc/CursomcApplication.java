4c505510a054058479c53af06e67d686e0a00508
Monica Luiza <moniluizad@gmail.com>
2022-04-14 11:43:47 -0300

    Criando instaciação das categorias

--- src/main/java/com/monica/cursomc/CursomcApplication.java
+++ src/main/java/com/monica/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.monica.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.monica.cursomc.domain.Categoria;
+import com.monica.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository rep;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		rep.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
